29961ee1af8937837dee0fb2c47d772d721e6519
Anova <anova.hou@gmail.com>
2021-10-14 11:13:54 -0700

    Phase 1 - added testGetGoalList to GoalTrackerTest to test more branches

--- src/test/model/GoalTrackerTest.java
+++ src/test/model/GoalTrackerTest.java
@@ -1,6 +1,10 @@
 package model;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import ui.GoalTrackerApp;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class GoalTrackerTest {
@@ -25,6 +29,15 @@ public class GoalTrackerTest {
         Goal g1 = new Goal("exercise", 3);
         testList.removeGoal(g1);
         assertEquals(0, testList.goalList.size());
+    }
+
+    @Test
+    void testGetGoalList() {
+        Goal g1 = new Goal("sleep", 1);
+        Goal g2 = new Goal( "study", 2);
+        testList.addGoal(g1);
+        testList.addGoal(g2);
+        assertEquals( 2, testList.getGoalList().size());
     }
 
 
