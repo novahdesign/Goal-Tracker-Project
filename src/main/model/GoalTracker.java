f151520734916395965553eb9a250509fea13209
Anova <anova.hou@gmail.com>
2021-10-15 13:31:19 -0700

    Phase 1 - Added class level comments to each class for better documentation

--- src/main/model/GoalTracker.java
+++ src/main/model/GoalTracker.java
@@ -11,13 +11,16 @@ public class GoalTracker {
         goalList = new ArrayList<>();
     }
 
-    // REQUIRES
-    // MODIFIES
-    // EFFECTS
+    // REQUIRES: Goal g not already in list
+    // MODIFIES: this
+    // EFFECTS: adds Goal g to the goalList
     public void addGoal(Goal g) {
         goalList.add(g);
     }
 
+    // REQUIRES: goalList is not an empty list
+    // MODIFIES: this
+    // EFFECTS: Goal g is removed from the goalList
     public void removeGoal(Goal g) {
         goalList.remove(g);
     }
