c8fbfc33c1d6f32fd912f6ba45c5f1d92522294d
Anova <anova.hou@gmail.com>
2021-10-24 13:53:07 -0700

    Phase 2 - wrote JsonReaderTest tests

--- src/test/persistence/JsonReaderTest.java
+++ src/test/persistence/JsonReaderTest.java
@@ -1,53 +1,57 @@
-//package persistence;
-//
-//import model.Category;
-//import model.Thingy;
-//import model.WorkRoom;
-//import org.junit.jupiter.api.Test;
-//
-//import java.io.IOException;
-//import java.util.List;
-//
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//import static org.junit.jupiter.api.Assertions.fail;
-//
-//class JsonReaderTest extends JsonTest {
-//
-//    @Test
-//    void testReaderNonExistentFile() {
-//        JsonReader reader = new JsonReader("./data/noSuchFile.json");
-//        try {
-//            WorkRoom wr = reader.read();
-//            fail("IOException expected");
-//        } catch (IOException e) {
-//            // pass
-//        }
-//    }
-//
-//    @Test
-//    void testReaderEmptyWorkRoom() {
-//        JsonReader reader = new JsonReader("./data/testReaderEmptyWorkRoom.json");
-//        try {
-//            WorkRoom wr = reader.read();
-//            assertEquals("My work room", wr.getName());
-//            assertEquals(0, wr.numThingies());
-//        } catch (IOException e) {
-//            fail("Couldn't read from file");
-//        }
-//    }
-//
-//    @Test
-//    void testReaderGeneralWorkRoom() {
-//        JsonReader reader = new JsonReader("./data/testReaderGeneralWorkRoom.json");
-//        try {
-//            WorkRoom wr = reader.read();
-//            assertEquals("My work room", wr.getName());
-//            List<Thingy> thingies = wr.getThingies();
-//            assertEquals(2, thingies.size());
-//            checkThingy("needle", Category.STITCHING, thingies.get(0));
-//            checkThingy("saw", Category.WOODWORK, thingies.get(1));
-//        } catch (IOException e) {
-//            fail("Couldn't read from file");
-//        }
-//    }
-//}
+package persistence;
+
+import model.Goal;
+import model.GoalTracker;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+class JsonReaderTest extends JsonTest {
+
+    @Test
+    void testReaderNonExistentFile() {
+        JsonReader reader = new JsonReader("./data/fileDoesNotExist.json");
+        try {
+            GoalTracker goalTracker = reader.read();
+            fail("IOException expected");
+        } catch (IOException e) {
+            // pass
+        }
+    }
+
+    @Test
+    void testReaderEmptyGoalTracker() {
+        JsonReader reader = new JsonReader("./data/testReaderEmptyGoalTracker.json");
+        try {
+            GoalTracker goalTracker = reader.read();
+            assertEquals("Goal Tracker Test User", goalTracker.getUser());
+            assertEquals(0, goalTracker.getLength());
+        } catch (IOException e) {
+            fail("Couldn't read from file");
+        }
+    }
+
+    @Test
+    void testReaderNormalGoalTracker() {
+        JsonReader reader = new JsonReader("./data/testReaderNormalGoalTracker.json");
+
+        try {
+
+            GoalTracker goalTracker = reader.read();
+            assertEquals("Goal Tracker Test User", goalTracker.getUser());
+
+            List<Goal> goals = goalTracker.getGoalList();
+            assertEquals(2, goalTracker.getLength());
+            assertEquals("Goal Tracker Test User", goalTracker.getUser());
+            checkGoal("study", 0, goals.get(0));
+            checkGoal("sleep", 0, goals.get(1));
+
+        } catch (IOException e) {
+            fail("Couldn't read from file");
+        }
+    }
+}
