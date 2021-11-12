947a4acc451f88a5b6806a18ac58406ad4ebe934
Anova <anova.hou@gmail.com>
2021-11-12 13:42:53 -0800

    Phase 3 - a continuation of GUI development

--- src/main/ui/GUI.java
+++ src/main/ui/GUI.java
@@ -13,18 +13,18 @@ public class GUI {
         JFrame frame = new JFrame();
 
         // construct the JButton
-        JButton button = new JButton("Click");
+        JButton button = new JButton("Welcome to GoalTracker. Click Here to Enter");
 
         JPanel panel = new JPanel();
         // set border of the panel
-        panel.setBorder(BorderFactory.createEmptyBorder(500, 500, 500, 500));
+        panel.setBorder(BorderFactory.createEmptyBorder(50, 50, 50, 50));
         panel.setLayout(new GridLayout(1,1));
 
         Color background = new Color(255, 192, 203);
         panel.setBackground(background);
 
         panel.add(button);
-        button.setBackground(new Color(147, 0, 255));
+        button.setBackground(new Color(234, 203, 255));
 
         // set the panel border layout
         frame.add(panel, BorderLayout.CENTER);
@@ -33,7 +33,26 @@ public class GUI {
         frame.pack();
         frame.setVisible(true);
 
+        // gradient
+
+//            Color color1 = Color.RED;
+//            Color color2 = Color.GREEN;
+//
+//            int w = panel.getWidth();
+//            int h = panel.getHeight();
+//
+//
+//            g2d.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+//
+//
+//            GradientPaint gp = new GradientPaint(0, 0, color1, 0, h, color2);
+//            g2d.setPaint(gp);
+//            g2d.fillRect(0, 0, w, h);
+//
+//            GradientPaint gradient = new GradientPaint(0,0, background, 0, h, 0)
+//
     }
+
 
 
 }
