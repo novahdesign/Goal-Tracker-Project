587659e31b12527b45d4dcef53e31012cb684bef
Anova <anova.hou@gmail.com>
2021-11-18 18:01:50 -0800

    Phase 3 - Fixes

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -10,13 +10,6 @@ import java.net.MalformedURLException;
 // Main class that creates instance of GoalTrackerApp class
 public class Main {
     public static void main(String[] args) throws MalformedURLException {
-      //  new GoalTrackerApp();
-    //    Goal initGoal = new Goal("Testing Goal", 25,160);
-//        GoalTracker goalTracker = new GoalTracker("Default User");
         new GoalTrackerScreen();
-    //    new GoalDetailScreen(initGoal, goalTracker);
-
-      //  System.out.println(initGoal.getName());
-       // new TextDemo();
     }
 }
